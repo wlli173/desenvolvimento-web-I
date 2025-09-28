63715162d541f961c0d12b460c821282e14bc2dc
wlli173 <willighan173@gmail.com>
2025-09-28 19:22:29 -0300

    Js lista de exercicios praticos

---
+++ Js/lista-1/ex2/variaveis.js
@@ -0,0 +1,19 @@
+var var1 = "Valor variável"
+let var2 = "Valor mutável"
+const var3 = "Valor constante"
+
+console.log(var1);
+console.log(var2);
+console.log(var3);
+
+var1 = "Novo valor variável"
+var2 = "Novo valor mutável"
+// var3 = "Novo valor constante" // Isso causará um erro
+
+console.log(var1);
+console.log(var2);
+console.log(var3);
+
+// var var1 = "Outro valor variável" // Isso é permitido, mas não é recomendado
+// let var2 = "Outro valor mutável" // Isso causará um erro
+// const var3 = "Outro valor constante" // Isso causará um erro
